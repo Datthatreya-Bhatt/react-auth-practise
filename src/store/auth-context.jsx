3684a57e359d7faffd76f2f10a92b3810fba25ec
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2024-10-15 22:25:38 +0530

    auto logout implemented

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -8,20 +8,35 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem('token');
-  const [token, setToken] = useState(initialToken);
+  const initialToken = JSON.parse(localStorage.getItem('token'));
+  const [token, setToken] = useState(initialToken?.token);
 
   const userLoggedIn = !!token;
 
+  function autoLogout(){
+    const token = JSON.parse(localStorage.getItem('token'));
+    if(token?.time + 300000 < Date.now() ){
+      console.log(token.time, token.time < Date.now()+300000)
+      localStorage.removeItem('token');
+    }
+    setTimeout(()=>{
+      localStorage.removeItem('token')
+    }, 50000)
+    
+  }
+
+
   const loginHandler = (token) => {
     setToken(token);
-    localStorage.setItem('token', token);
+    const obj = {token, time: Date.now()};
+    localStorage.setItem('token', JSON.stringify(obj));
   };
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
   };
+  autoLogout();
 
   const contextValue = {
     token: token,
